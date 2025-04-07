1a4af4e2eec3195ecfe2aa2f1f7c6547c0bd8b12
Juan Manuel Fernandes dos Santos <juan9794@gmail.com>
2025-04-07 16:21:53 -0300

    Codigo clase 2

--- clase-02/src/Library.hs
+++ clase-02/src/Library.hs
@@ -3,8 +3,54 @@ module Library (module Library, isPrefixOf, isSuffixOf) where
 import PdePreludat
 import Data.List (isPrefixOf, isSuffixOf)
 
+celsiusAFahrenheit :: Number -> Number
+celsiusAFahrenheit temperaturaEnCelsius =
+    temperaturaEnCelsius * 1.8 + 32
+
+fahrenheitACelsius :: Number -> Number
+fahrenheitACelsius temperaturaEnFahrenheit =
+    (temperaturaEnFahrenheit - 32) / 1.8
+
+haceFrioCelsius :: Number -> Bool
+haceFrioCelsius temperaturaEnCelsius =
+    temperaturaEnCelsius <= 8
+
+-- even 5 == odd 6
+
+-- haceFrioFahrenheit :: Number -> Bool
+-- haceFrioFahrenheit temperaturaEnFahrenheit =
+--     temperaturaEnFahrenheit <= celsiusAFahrenheit 8
+
+haceFrioFahrenheit' :: Number -> Bool
+haceFrioFahrenheit' temperaturaEnFahrenheit =
+    haceFrioCelsius (fahrenheitACelsius temperaturaEnFahrenheit)
+
+-- haceFrioFahrenheit'' :: Number -> Bool
+-- haceFrioFahrenheit'' temperaturaEnFahrenheit =
+--     fahrenheitACelsius temperaturaEnFahrenheit <= 8
+
+
+-- preguntar :: String -> String
+-- preguntar oracion
+--     | isPrefixOf "¿" oracion && isSuffixOf "?" oracion = oracion
+--     | "¿" `isPrefixOf` oracion = oracion ++ "?"
+--     | "?" `isSuffixOf` oracion = "¿" ++ oracion
+--     | otherwise = "¿" ++ oracion ++ "?"
+
 preguntar :: String -> String
-preguntar oracion = ""
+preguntar oracion =
+    agregarSiFaltaAlFinal "?" 
+        (agregarSiFaltaAlPrincipio "¿" oracion)
+
+agregarSiFaltaAlPrincipio :: String -> String -> String
+agregarSiFaltaAlPrincipio prefijo oracion
+    | prefijo `isPrefixOf` oracion = oracion
+    | otherwise = prefijo ++ oracion
+
+agregarSiFaltaAlFinal :: String -> String -> String
+agregarSiFaltaAlFinal sufijo oracion
+    | sufijo `isSuffixOf` oracion = oracion
+    | otherwise = oracion ++ sufijo
 
 -- isPrefixOf nos dice si un string empieza con otro string
 -- isSuffixOf nos dice si un string termina con otro string
@@ -21,6 +67,5 @@ preguntar oracion = ""
 
 -- Queremos que funcione así:
 -- >>> preguntar "hola"
--- "hola?"
--- >>> preguntar "hola?"
--- "hola?"
+-- "¿hola?"-- >>> preguntar "hola?"
+-- "¿hola?"
