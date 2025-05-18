07fd7ceb76890765050efc7934792a7ca0539f9b
Juan Manuel Fernandes dos Santos <jfdossantos@frba.utn.edu.ar>
2025-05-18 17:06:35 -0300

    Agregado calculo lambda a clase 7

--- clase-07/src/Library.hs
+++ clase-07/src/Library.hs
@@ -73,3 +73,71 @@ fry :: Persona
 fry = Persona "Philip J Fry" [sherri, yancy]
 yancy :: Persona
 yancy = Persona "Yancy Fry" [mildred, fry]
+
+
+-- calculo lambda
+aHaskell = \b -> b True False
+
+verdadero = \x -> \y -> x
+            -- λx . λy . x
+
+falso = \x -> \y -> y
+
+no = \b -> b falso verdadero
+
+o = \b1 -> \b2 ->
+    b1 verdadero b2
+
+sumar :: Number -> Number -> Number
+sumar x y = x + y
+sumar' :: Number -> Number -> Number
+sumar' x = \y -> x + y
+sumar'' :: Number -> Number -> Number
+sumar'' = \x -> \y -> x + y 
+-- \x -> x + 2
+
+-- no = \b -> b verdadero falso
+
+-- -- y true false = false
+-- -- y false false = false
+-- -- y false true = false
+-- -- y true true = true
+
+-- y = \b1 -> \b2 -> b1 b2 falso
+
+-- -- o true false = true
+-- -- o false false = false
+-- -- o false true = false
+-- -- o true true = true
+
+-- o = \b1 -> \b2 -> b1 verdadero b2 verdadero falso
+
+-- si = \b -> b
+
+-- -- true == false = false
+-- -- true == true = true
+-- -- false == true = false
+-- -- false == false = true
+
+
+-- sonIguales = \b1 -> \b2 -> b1 b2 (no b2) verdadero falso
+
+
+-- sonDistintos = \b1 -> \b2 -> no (sonIguales b1 b2)
+
+
+-- esVerdadero = \b1 -> sonIguales b1 verdadero
+
+
+-- esFalso = \b1 -> sonIguales b1 falso
+
+-- -- true > false = true
+-- -- false > true = false
+-- -- true > true = false
+-- -- false > false = false
+
+-- (.>) = \b1 -> \b2 -> esVerdadero b1 `y` esFalso b2
+
+-- (.<) = \b1 -> \b2 -> esFalso b1 `y` esVerdadero b2
+
+-- aHaskell = \b -> b True False
